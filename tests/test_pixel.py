fbda18bad01ed6c428fba0f817e9ccd96bdb60aa
Stefan Hagen <stefan@hagen.link>
2021-05-09 15:20:55 +0200

    Corrected copy-pasta of pixel test names

--- tests/test_pixel.py
+++ tests/test_pixel.py
@@ -18,7 +18,7 @@ RGB_RED_NAME = "ff0000_2x2.png"
 REF_CHILD_RGB_RED_PNG = pathlib.Path(REF_CHILD_FOLDER, RGB_RED_NAME)
 
 
-def test_visit_ok_test_single_fixture_rgb_file():
+def test_shape_of_png_ok_test_single_fixture_rgb_file():
     ok, width, height, info = shape_of_png(REF_CHILD_RGB_RED_PNG)
     assert ok is True
     assert width == 2 and height == 2
@@ -36,14 +36,14 @@ def test_visit_ok_test_single_fixture_rgb_file():
     assert info == facts
 
 
-def test_visit_nok_test_single_fixture_non_png_file():
+def test_shape_of_png_nok_test_single_fixture_non_png_file():
     ok, width, height, info = shape_of_png(SINGLE_FILE_PATH_EMPTY_PNG)
     assert ok is False
     assert width is None and height is None
     assert info["error"].lower() == "formaterror: png file has invalid signature."
 
 
-def test_visit_nok_non_existing_file():
+def test_shape_of_png_nok_non_existing_file():
     file_not_there = pathlib.Path("no", "file", "here")
     assert not file_not_there.exists(), f"WARNING: The path {file_not_there} SHOULD not exist, but does."
     ok, width, height, info = shape_of_png(file_not_there)
