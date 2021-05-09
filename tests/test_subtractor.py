3c126d80de39ba332f7abe0beb2578a70347fab4
Stefan Hagen <stefan@hagen.link>
2021-05-09 15:49:50 +0200

    Added negative but silent slugify use case with empty source and target

--- tests/test_subtractor.py
+++ tests/test_subtractor.py
@@ -51,3 +51,8 @@ def test_slugify_as_to_bs_and_cs_mok():
     with pytest.raises(ValueError):
         slugify("aaabb", these=("a",), those=("b", "c"))
 
+
+def test_slugify_nok_with_single_newline_but_empty_source_and_target():
+    text = "one line\nanother line"
+    assert slugify(text, these=[], those=[]) == text
+
