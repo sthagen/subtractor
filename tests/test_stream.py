62f6f3c72dab7332dee6cce16cc91c1a9b93493c
Stefan Hagen <stefan@hagen.link>
2021-05-09 10:57:46 +0200

    Added test for platform dependend order of retrieval from fs calls

--- tests/test_stream.py
+++ tests/test_stream.py
@@ -8,7 +8,7 @@ import pytest  # type: ignore
 from subtractor.stream import visit
 
 
-def test_visit_ok_test_fixture_file():
+def test_visit_ok_test_single_fixture_file():
     empty_png = pathlib.Path("tests", "fixtures", "single_file", "empty.png")
     visitor = visit(empty_png)
     assert isinstance(visitor, Generator)
@@ -17,11 +17,25 @@ def test_visit_ok_test_fixture_file():
         next(visitor)
 
 
-def test_visit_ok_test_fixture_folder_with_wun_file():
+def test_visit_ok_test_fixture_folder_with_single_file():
     single_file_folder = pathlib.Path("tests", "fixtures", "single_file")
     empty_png = pathlib.Path(single_file_folder, "empty.png")
     visitor = visit(single_file_folder)
     assert isinstance(visitor, Generator)
     assert next(visitor) == empty_png
+    with pytest.raises(StopIteration):
+        next(visitor)
+
+
+def test_visit_ok_test_fixture_folder_with_two_files():
+    two_files_folder = pathlib.Path("tests", "fixtures", "two_files")
+    empty_png = pathlib.Path(two_files_folder, "empty.png")
+    another_empty_png = pathlib.Path(two_files_folder, "another_empty.png")
+    visitor = visit(two_files_folder)
+    assert isinstance(visitor, Generator)
+    first = next(visitor)
+    assert first in (another_empty_png, empty_png)
+    last = next(visitor)
+    assert last != first and last in (another_empty_png, empty_png)
     with pytest.raises(StopIteration):
         next(visitor)
