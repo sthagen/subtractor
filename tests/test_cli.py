0c3f1d34c8d86a21b2f11692727f19a6ffe9c6ba
Stefan Hagen <stefan@hagen.link>
2021-05-09 16:05:13 +0200

    Added CLI test taking the valid size branch in subtractor main

--- tests/test_cli.py
+++ tests/test_cli.py
@@ -13,6 +13,12 @@ DEFAULT_FILE_NAME = "empty.png"
 SINGLE_FILE_FOLDER = pathlib.Path(FIXTURE_ROOT, "single_file")
 SINGLE_FILE_PATH_EMPTY_PNG = pathlib.Path(SINGLE_FILE_FOLDER, DEFAULT_FILE_NAME)
 
+REF_OBS_ROOT = pathlib.Path(FIXTURE_ROOT, "ref_obs")
+REF_CHILD_FOLDER = pathlib.Path(REF_OBS_ROOT, "ref")
+OBS_CHILD_FOLDER = pathlib.Path(REF_OBS_ROOT, "obs")
+RGB_RED_NAME = "ff0000_2x2.png"
+REF_CHILD_RGB_RED_PNG = pathlib.Path(REF_CHILD_FOLDER, RGB_RED_NAME)
+
 
 def test_main_ok_no_args(capsys):
     assert cli.main([], debug=False) == 0
@@ -21,7 +27,7 @@ def test_main_ok_no_args(capsys):
     assert not err
 
 
-def test_main_ok_test_fixtures_single_file(caplog, capsys):
+def test_main_nok_test_fixtures_single_non_png_file(caplog, capsys):
     caplog.set_level(logging.INFO)
     assert cli.main([SINGLE_FILE_FOLDER], debug=False) == 0
     out, err = capsys.readouterr()
@@ -30,3 +36,17 @@ def test_main_ok_test_fixtures_single_file(caplog, capsys):
     assert "starting comparisons visiting a forest with 1 tree" in caplog.text.lower()
     assert f"found {SINGLE_FILE_PATH_EMPTY_PNG} to be nok with size 0 bytes" in caplog.text.lower()
     assert f"analyzed {SINGLE_FILE_PATH_EMPTY_PNG} as png to be nok with formaterror: png file has invalid signature." in caplog.text.lower()
+
+
+def test_main_ok_test_fixtures_single_png_file(caplog, capsys):
+    caplog.set_level(logging.INFO)
+    assert cli.main([REF_CHILD_RGB_RED_PNG], debug=False) == 0
+    out, err = capsys.readouterr()
+    assert "ok" in out.lower()
+    assert not err
+    lines = caplog.text.lower().split("\n")
+    expected_log_line_count = 4
+    assert len(lines) == expected_log_line_count and not lines[expected_log_line_count - 1]
+    assert "starting comparisons visiting a forest with 1 tree" in lines[0]
+    assert f"found {REF_CHILD_RGB_RED_PNG} to be ok with size 277 bytes" in lines[1]
+    assert f"analyzed {REF_CHILD_RGB_RED_PNG} as png to be ok with shape 2x2" in lines[2]
