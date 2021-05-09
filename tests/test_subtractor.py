f7050ca5749ab6a948d5ea0695014cb1ec4b5e8e
Stefan Hagen <stefan@hagen.link>
2021-05-09 15:43:45 +0200

    Added another slugify test

--- tests/test_subtractor.py
+++ tests/test_subtractor.py
@@ -39,5 +39,10 @@ def test_slugify_ok_with_single_newline():
     assert slugify("one line\nanother line") == "one line another line"
 
 
+def test_slugify_as_to_bs_ok():
+    assert slugify("aaabb", these=("a",), those=("b",)) == "bbbbb"
+
+
 def test_slugify_as_and_bs_to_cs_ok():
     assert slugify("aaabb", these=("a", "b"), those=("c",)) == "ccccc"
+
