f5a60fdb3582205d15ffe4fa9f17067b62126e2f
Stefan Hagen <stefan@hagen.link>
2021-05-09 11:09:57 +0200

    Further de-literal-ing

--- tests/test_stream.py
+++ tests/test_stream.py
@@ -7,14 +7,17 @@ import pytest  # type: ignore
 
 from subtractor.stream import visit
 
-TWO_FILES_FOLDER = pathlib.Path("tests", "fixtures", "two_files")
-EMPTY_PNG = pathlib.Path(TWO_FILES_FOLDER, "empty.png")
+FIXTURE_ROOT = pathlib.Path("tests", "fixtures")
+DEFAULT_FILE_NAME = "empty.png"
+SINGLE_FILE_FOLDER = pathlib.Path(FIXTURE_ROOT, "single_file")
+TWO_FILES_FOLDER = pathlib.Path(FIXTURE_ROOT, "two_files")
+EMPTY_PNG = pathlib.Path(TWO_FILES_FOLDER, DEFAULT_FILE_NAME)
 ANOTHER_EMPTY_PNG = pathlib.Path(TWO_FILES_FOLDER, "another_empty.png")
 TWO_FILES_SET = {ANOTHER_EMPTY_PNG, EMPTY_PNG}
 
 
 def test_visit_ok_test_single_fixture_file():
-    empty_png = pathlib.Path("tests", "fixtures", "single_file", "empty.png")
+    empty_png = pathlib.Path(SINGLE_FILE_FOLDER, DEFAULT_FILE_NAME)
     visitor = visit(empty_png)
     assert isinstance(visitor, Generator)
     assert next(visitor) == empty_png
@@ -23,8 +26,8 @@ def test_visit_ok_test_single_fixture_file():
 
 
 def test_visit_ok_test_fixture_folder_with_single_file():
-    single_file_folder = pathlib.Path("tests", "fixtures", "single_file")
-    empty_png = pathlib.Path(single_file_folder, "empty.png")
+    single_file_folder = pathlib.Path(SINGLE_FILE_FOLDER)
+    empty_png = pathlib.Path(single_file_folder, DEFAULT_FILE_NAME)
     visitor = visit(single_file_folder)
     assert isinstance(visitor, Generator)
     assert next(visitor) == empty_png
