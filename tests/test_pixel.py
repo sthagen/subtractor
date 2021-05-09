79da9dd79b60ce521a04859ccd8d6b7fbf0a19b7
Stefan Hagen <stefan@hagen.link>
2021-05-09 13:43:32 +0200

    Added negative test for pixel shape detector on non existing file

--- tests/test_pixel.py
+++ tests/test_pixel.py
@@ -28,3 +28,10 @@ def test_visit_nok_test_single_fixture_non_png_file():
     ok, message = shape_of_png(SINGLE_FILE_PATH_EMPTY_PNG)
     assert ok is False
     assert message.lower() == "formaterror: png file has invalid signature."
+
+
+def test_visit_nok_non_existing_file():
+    file_not_there = pathlib.Path("no", "file", "here")
+    ok, message = shape_of_png(file_not_there)
+    assert ok is False
+    assert message.lower() == f"[errno 2] no such file or directory: '{str(file_not_there).lower()}'"
