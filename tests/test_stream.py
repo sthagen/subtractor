a711dde73591ba2bb253458440bd24bddd9f712b
Stefan Hagen <stefan@hagen.link>
2021-05-09 10:50:04 +0200

    Adapted stream tests to changed  fixtures

--- tests/test_stream.py
+++ tests/test_stream.py
@@ -9,7 +9,7 @@ from subtractor.stream import visit
 
 
 def test_visit_ok_test_fixture_file():
-    empty_png = pathlib.Path("tests", "fixtures", "empty.png")
+    empty_png = pathlib.Path("tests", "fixtures", "single_file", "empty.png")
     visitor = visit(empty_png)
     assert isinstance(visitor, Generator)
     assert next(visitor) == empty_png
@@ -18,7 +18,7 @@ def test_visit_ok_test_fixture_file():
 
 
 def test_visit_ok_test_fixture_folder_with_wun_file():
-    single_file_folder = pathlib.Path("tests", "fixtures")
+    single_file_folder = pathlib.Path("tests", "fixtures", "single_file")
     empty_png = pathlib.Path(single_file_folder, "empty.png")
     visitor = visit(single_file_folder)
     assert isinstance(visitor, Generator)
