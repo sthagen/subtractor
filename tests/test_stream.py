68571b2a7227944650e8f071f710ddb08ef8d99f
Stefan Hagen <stefan@hagen.link>
2021-05-09 11:06:46 +0200

    Refactored test suite for streams

--- tests/test_stream.py
+++ tests/test_stream.py
@@ -7,6 +7,11 @@ import pytest  # type: ignore
 
 from subtractor.stream import visit
 
+TWO_FILES_FOLDER = pathlib.Path("tests", "fixtures", "two_files")
+EMPTY_PNG = pathlib.Path(TWO_FILES_FOLDER, "empty.png")
+ANOTHER_EMPTY_PNG = pathlib.Path(TWO_FILES_FOLDER, "another_empty.png")
+TWO_FILES_SET = set([ANOTHER_EMPTY_PNG, EMPTY_PNG])
+
 
 def test_visit_ok_test_single_fixture_file():
     empty_png = pathlib.Path("tests", "fixtures", "single_file", "empty.png")
@@ -28,38 +33,29 @@ def test_visit_ok_test_fixture_folder_with_single_file():
 
 
 def test_visit_ok_test_fixture_folder_with_two_files():
-    two_files_folder = pathlib.Path("tests", "fixtures", "two_files")
-    empty_png = pathlib.Path(two_files_folder, "empty.png")
-    another_empty_png = pathlib.Path(two_files_folder, "another_empty.png")
-    visitor = visit(two_files_folder)
+    visitor = visit(TWO_FILES_FOLDER)
     assert isinstance(visitor, Generator)
     first = next(visitor)
-    assert first in (another_empty_png, empty_png)
+    assert first in (ANOTHER_EMPTY_PNG, EMPTY_PNG)
     last = next(visitor)
-    assert last != first and last in (another_empty_png, empty_png)
+    assert last != first and last in TWO_FILES_SET
     with pytest.raises(StopIteration):
         next(visitor)
 
 
 def test_visit_sorted_ok_test_fixture_folder_with_two_files():
-    two_files_folder = pathlib.Path("tests", "fixtures", "two_files")
-    empty_png = pathlib.Path(two_files_folder, "empty.png")
-    another_empty_png = pathlib.Path(two_files_folder, "another_empty.png")
-    visitor = visit(two_files_folder, a_filter=sorted)
+    visitor = visit(TWO_FILES_FOLDER, a_filter=sorted)
     assert isinstance(visitor, Generator)
-    assert next(visitor) == another_empty_png
-    assert next(visitor) == empty_png
+    assert next(visitor) == ANOTHER_EMPTY_PNG
+    assert next(visitor) == EMPTY_PNG
     with pytest.raises(StopIteration):
         next(visitor)
 
 
 def test_visit_sorted_reverse_ok_test_fixture_folder_with_two_files():
-    two_files_folder = pathlib.Path("tests", "fixtures", "two_files")
-    empty_png = pathlib.Path(two_files_folder, "empty.png")
-    another_empty_png = pathlib.Path(two_files_folder, "another_empty.png")
-    visitor = visit(two_files_folder, a_filter=sorted, filter_options=dict(reverse=True))
+    visitor = visit(TWO_FILES_FOLDER, a_filter=sorted, filter_options=dict(reverse=True))
     assert isinstance(visitor, Generator)
-    assert next(visitor) == empty_png
-    assert next(visitor) == another_empty_png
+    assert next(visitor) == EMPTY_PNG
+    assert next(visitor) == ANOTHER_EMPTY_PNG
     with pytest.raises(StopIteration):
         next(visitor)
