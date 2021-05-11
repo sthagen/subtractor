255b4b14d5f36772efad1233c03c794ebcdf13e8
Stefan Hagen <stefan@hagen.link>
2021-05-11 18:53:55 +0200

    Added CLI test for fast enlargement of test coverage from around 40 to nearly 80 % of subtractor module

--- tests/test_cli.py
+++ tests/test_cli.py
@@ -18,6 +18,7 @@ REF_CHILD_FOLDER = pathlib.Path(REF_OBS_ROOT, "ref")
 OBS_CHILD_FOLDER = pathlib.Path(REF_OBS_ROOT, "obs")
 RGB_RED_NAME = "ff0000_2x2.png"
 REF_CHILD_RGB_RED_PNG = pathlib.Path(REF_CHILD_FOLDER, RGB_RED_NAME)
+OBS_CHILD_RGB_RED_PNG = pathlib.Path(OBS_CHILD_FOLDER, RGB_RED_NAME)
 
 
 def test_main_ok_no_args(capsys):
@@ -53,3 +54,28 @@ def test_main_nok_test_fixtures_single_png_file(caplog, capsys):
     assert not err
     lines = caplog.text.lower().split("\n")
     expected_log_line_count = 1
+    assert len(lines) == expected_log_line_count
+    assert not lines[0].strip()
+
+
+def test_main_ok_test_fixtures_matching_png_files(caplog, capsys):
+    caplog.set_level(logging.INFO)
+    assert cli.main([REF_CHILD_RGB_RED_PNG, OBS_CHILD_RGB_RED_PNG], debug=False) == 0
+    out, err = capsys.readouterr()
+    assert "ok" == out.lower().strip()
+    assert not err
+    lines = caplog.text.lower().split("\n")
+    expected_log_line_count = 10
+    assert len(lines) == expected_log_line_count
+    assert "starting comparisons visiting past" in lines[0].lower()
+    assert "file mode" in lines[0].lower()
+    assert "threshold for pixel mismatch is 1 %" in lines[1].lower()
+    assert "pair" in lines[2].lower()
+    for ndx in (3, 5):
+        assert "to be ok with size 277 bytes" in lines[ndx].lower()
+    for ndx in (4, 6):
+        assert "as png to be ok with shape 2x2" in lines[ndx].lower()
+    assert f"match of obs={str(OBS_CHILD_RGB_RED_PNG)}" in lines[7].lower()
+    assert "finished comparisons finding good=1 and bad=0 in file mode" in lines[8].lower()
+    assert not lines[9].strip()
+
