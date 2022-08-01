b7a7f38328123228e621dd14b622c887f9f51134
Stefan Hagen <stefan@hagen.link>
2022-08-01 19:08:39 +0200

    Bumped impl version for hot fix

--- subtractor/__init__.py
+++ subtractor/__init__.py
@@ -15,8 +15,8 @@ DEFAULT_CONFIG_NAME = '.subtractor.json'
 DEFAULT_LF_ONLY = 'YES'
 
 # [[[fill git_describe()]]]
-__version__ = '2022.8.2+parent.8746c51c'
-# [[[end]]] (checksum: 15ad1d05d39dbfef6ff464be1d255c8a)
+__version__ = '2022.8.2+parent.1d9ba9cc'
+# [[[end]]] (checksum: 2fed94e583e75ddcda18db50b9126c1c)
 __version_info__ = tuple(
     e if '-' not in e else e.split('-')[0] for part in __version__.split('+') for e in part.split('.') if e != 'parent'
 )
