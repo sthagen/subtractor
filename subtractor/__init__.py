1f2604e65b41553ed29e1bc362f97110a958f245
Stefan Hagen <stefan@hagen.link>
2023-07-01 18:00:22 +0200

    Derived some app specific variable values from module (well file) docstring and the place

--- subtractor/__init__.py
+++ subtractor/__init__.py
@@ -1,17 +1,19 @@
 """Pixels, pixels, pixels."""
 import os
+import pathlib
 from typing import List
 
-APP_NAME = 'Pixels, pixels, pixels.'
-APP_ALIAS = 'subtractor'
-APP_ENV = 'SUBTRACTOR'
+APP_ALIAS = str(pathlib.Path(__file__).parent.name)
+APP_ENV = APP_ALIAS.upper()
+APP_NAME = locals()['__doc__']
 DEBUG = bool(os.getenv(f'{APP_ENV}_DEBUG', ''))
 VERBOSE = bool(os.getenv(f'{APP_ENV}_VERBOSE', ''))
 QUIET = False
 STRICT = bool(os.getenv(f'{APP_ENV}_STRICT', ''))
 ENCODING = 'utf-8'
 ENCODING_ERRORS_POLICY = 'ignore'
-DEFAULT_CONFIG_NAME = '.subtractor.json'
+DEFAULT_CONFIG_NAME = f'.{APP_ALIAS}.json'
+
 DEFAULT_LF_ONLY = 'YES'
 
 # [[[fill git_describe()]]]
