e32fb3a5445af982715aa67be619b3390d12221c
Stefan Hagen <stefan@hagen.link>
2023-10-21 16:00:15 +0200

    Bumped impl version for release 2023.10.21

--- subtractor/__init__.py
+++ subtractor/__init__.py
@@ -17,8 +17,8 @@ DEFAULT_CONFIG_NAME = f'.{APP_ALIAS}.json'
 DEFAULT_LF_ONLY = 'YES'
 
 # [[[fill git_describe()]]]
-__version__ = '2022.8.2+parent.1d9ba9cc'
-# [[[end]]] (checksum: 2fed94e583e75ddcda18db50b9126c1c)
+__version__ = '2023.10.21+parent.0e0af55d'
+# [[[end]]] (checksum: 491e561bedc1b37829255dfa7e3ac2a4)
 __version_info__ = tuple(
     e if '-' not in e else e.split('-')[0] for part in __version__.split('+') for e in part.split('.') if e != 'parent'
 )
